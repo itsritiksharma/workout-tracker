a3693c2c9719ea388dfd9fcb9ce6b47816751874
itsritiksharma <sritik216@gmail.com>
2022-02-07 18:31:42 +0530

    Added auth provider to the main and rendered screen on auth state.

--- lib/main.dart
+++ lib/main.dart
@@ -1,73 +1,90 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 import './screens/calendar_screen.dart';
 import './widgets/home_page.dart';
 import 'providers/app_mode.dart';
 import './providers/app_mode.dart';
+import './screens/auth_screen.dart';
+import './providers/auth.dart';
 
-void main() =>
-    runApp(ChangeNotifierProvider(create: (ctx) => AppMode(), child: MyApp()));
+void main() async {
+  await dotenv.load();
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    // final app = Provider.of<AppMode>(context);
-    // final appMode = app.appMode;
-    // print(appMode);
-    return Consumer<AppMode>(
-      builder: (_, mode, __) {
-        return MaterialApp(
-          darkTheme: ThemeData(
-            brightness: Brightness.dark,
-            primaryColor: Colors.black,
-            accentColor: Colors.orange,
-            errorColor: Colors.red,
-            // canvasColor:
-            // colorScheme: ThemeData().colorScheme.copyWith(
-            //     secondary: Colors.orange,
-            //     primary: Colors.black,
-            //     brightness: Brightness.dark,
-            //     error: Colors.red),
-          ),
-          themeMode: mode.appMode,
-          debugShowCheckedModeBanner: false,
-          title: 'Workout Tracker',
-          theme: ThemeData(
-            // colorScheme: ThemeData().colorScheme.copyWith(
-            //     secondary: Colors.orange,
-            //     secondaryVariant: Colors.orange,
-            //     primary: Colors.deepOrange,
-            //     error: Colors.red,
-            //     brightness: Brightness.light),
-            primarySwatch: Colors.deepOrange,
-            errorColor: Colors.red,
-            accentColor: Colors.orange,
-            // canvasColor: Color.fromRGBO(255, 254, 229, 1),
-            canvasColor: Color.fromRGBO(255, 255, 255, 1),
-            // fontFamily: 'Raleway',
-            textTheme: ThemeData.light().textTheme.copyWith(
-                  bodyText1: TextStyle(
-                    color: Color.fromRGBO(20, 51, 51, 1),
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider.value(
+          value: Auth(),
+        ),
+        ChangeNotifierProvider.value(
+          value: AppMode(),
+        )
+      ],
+      child: Consumer<AppMode>(
+        builder: (_, mode, __) {
+          return MaterialApp(
+            darkTheme: ThemeData(
+              brightness: Brightness.dark,
+              primaryColor: Colors.black,
+              accentColor: Colors.orange,
+              errorColor: Colors.red,
+              // canvasColor:
+              // colorScheme: ThemeData().colorScheme.copyWith(
+              //     secondary: Colors.orange,
+              //     primary: Colors.black,
+              //     brightness: Brightness.dark,
+              //     error: Colors.red),
+            ),
+            themeMode: mode.appMode,
+            debugShowCheckedModeBanner: false,
+            title: 'Workout Tracker',
+            theme: ThemeData(
+              // colorScheme: ThemeData().colorScheme.copyWith(
+              //     secondary: Colors.orange,
+              //     secondaryVariant: Colors.orange,
+              //     primary: Colors.deepOrange,
+              //     error: Colors.red,
+              //     brightness: Brightness.light),
+              primarySwatch: Colors.deepOrange,
+              errorColor: Colors.red,
+              accentColor: Colors.orange,
+              // canvasColor: Color.fromRGBO(255, 254, 229, 1),
+              canvasColor: Color.fromRGBO(255, 255, 255, 1),
+              // fontFamily: 'Raleway',
+              textTheme: ThemeData.light().textTheme.copyWith(
+                    bodyText1: TextStyle(
+                      color: Color.fromRGBO(20, 51, 51, 1),
+                    ),
+                    bodyText2: TextStyle(
+                      color: Color.fromRGBO(20, 51, 51, 1),
+                    ),
+                    headline6: TextStyle(
+                      fontSize: 20,
+                      // fontFamily: 'RobotoCondensed',
+                      fontWeight: FontWeight.bold,
+                    ),
                   ),
-                  bodyText2: TextStyle(
-                    color: Color.fromRGBO(20, 51, 51, 1),
-                  ),
-                  headline6: TextStyle(
-                    fontSize: 20,
-                    // fontFamily: 'RobotoCondensed',
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-          ),
-          routes: {
-            '/': (_) => HomePage(),
-            CalendarScreen.routName: (_) => CalendarScreen()
-          },
-        );
-      },
+            ),
+            home: Consumer<Auth>(
+              builder: (ctx, auth, __) {
+                return auth.isAuth ? HomePage() : AuthScreen();
+              },
+            ),
+            routes: {
+              // '/': (_) => HomePage(),
+              CalendarScreen.routName: (_) => CalendarScreen()
+            },
+          );
+        },
+      ),
     );
   }
 }
